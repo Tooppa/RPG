2805193daca6e72391b83a23e1fa8bf2da710c6c
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-12 12:06:41 +0200

    Bases for other charecters

---
+++ Charecters/Rogue.cs
@@ -0,0 +1,10 @@
+﻿namespace RPG.Charecters
+{
+    internal class Rogue : Charecter
+    {
+        public override void LevelUp(int strength, int dexterity, int intelligence)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Charecters/Warrior.cs
@@ -0,0 +1,10 @@
+﻿namespace RPG.Charecters
+{
+    internal class Warrior : Charecter
+    {
+        public override void LevelUp(int strength, int dexterity, int intelligence)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
